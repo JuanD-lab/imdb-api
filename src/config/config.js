1e475c07cec5f2a47b7e0ae916e8c179aadff500
JuanD-lab <yo-juan@outlook.com>
2021-10-04 18:33:20 -0500

    edit config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,27 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
-  "test": {
+  development: {
+    use_env_variable: 'DEV_DATABASE_URL',
+},
+test: {
     use_env_variable: 'TEST_DATABASE_URL',
-  },
+},
   "production": {
     use_env_variable: 'DATABASE_URL',
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     },
-}  
+  }  
 }
+
+/* "development": {
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": "127.0.0.1",
+  "dialect": "postgres"
+}, */
