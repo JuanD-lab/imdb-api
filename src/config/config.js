7a660bded43d08e684405437f8568300b8d7324a
JuanD-lab <yo-juan@outlook.com>
2021-10-04 17:42:33 -0500

    edit config envs

--- src/config/config.js
+++ src/config/config.js
@@ -9,17 +9,15 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    use_env_variable: 'TEST_DATABASE_URL',
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    use_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+}  
 }
