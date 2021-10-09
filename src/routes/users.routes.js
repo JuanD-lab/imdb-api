0ed42b9f88a39918d781d3ca4f1a30d6f404cf57
JuanD-lab <yo-juan@outlook.com>
2021-10-09 17:24:05 -0500

    add auth to list users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express')
 const route = Router()
+const {verifyToken } = require('../middlewares/auth.middleware')
 //Methods
 const {list, create, update, destroy, verify} = require('../controllers/users.controller')
 
 //Endpoints
-route.get("/users", list)//List
+route.get("/users", verifyToken, list)//List
 route.post("/users", create)//Create
 route.put("/users/:id", update)//Update
 route.delete("/users/:id", destroy)//Delete
